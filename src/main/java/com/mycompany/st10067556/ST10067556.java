d22d19c1aca4df1fb9ca3e803693fe0fb6b74574
Drage66 <nhlanhlazuma123@gmail.com>
2022-04-19 20:27:25 +0200

    Yes

--- src/main/java/com/mycompany/st10067556/ST10067556.java
+++ src/main/java/com/mycompany/st10067556/ST10067556.java
@@ -3,33 +3,13 @@
 package com.mycompany.st10067556;
 
 import javax.swing.*;
+import java.io.*;
 import java.util.Scanner;
 import java.util.regex.*;
 
 
 public class ST10067556
 {
-//    public static boolean checkUserName(String userNames)
-//    {
-//        boolean verify; // this verifies input
-//        Pattern input = Pattern.compile("_+"); //Pattern object created to search for the single character underscore
-//        Matcher matcher = input.matcher(userNames); //Matcher object derived from pattern object that looks in the input for the patter
-//        boolean matchFound = matcher.find(); // returns true if underscore found and false if not
-//
-//        int charCount = userNames.length(); //returns the amount of characters in the string. Can only be created as long for some reason
-//
-//        if (matchFound && charCount <= 5) //Checks whether the number of characters is less than or equal to 5 AND whether the matchfound returned true
-//        {
-//            verify = true;
-//        }
-//        else
-//        {
-//            verify = false;
-//        }
-//
-//        return verify; //returns true or false
-//    }
-
     public static boolean checkUserName(String userName)
     {
         int locator = 0;
@@ -51,33 +31,9 @@ public class ST10067556
             }
         }
 
-
         return find;
 
     }
-
-//    public static boolean checkPasswordComplexity(String password)
-//    {
-//        boolean verify; // this verifies input
-//        String character = " ";;
-//        Pattern input = Pattern.compile(character); //Pattern object created to search for the single character underscore
-//        Matcher matcher = input.matcher(password); //Matcher object derived from pattern object that looks in the input for the pattern
-//        boolean matchFound = matcher.find(); // returns true if underscore found and false if not
-//
-//        int charCount = password.length(); //returns the amount of characters in the string. Can only be created as long for some reason
-//
-//        if (matchFound && charCount >= 8) //Checks whether the number of characters is greater than or equal to 8 AND whether the matchfound returned true
-//        {
-//            verify = true;
-//        }
-//        else
-//        {
-//            verify = false;
-//        }
-//
-//        return verify; //returns true or false
-//
-//    }
 
     public static boolean checkPasswordComplexity(String password)
     {
@@ -98,7 +54,7 @@ public class ST10067556
                 break;
             }
 
-            int charValue = (int) password.charAt(locator);
+            int charValue = password.charAt(locator);
 
             if (charValue >= 65 && charValue <= 90)// looks for Caps and returns true if found
             {
@@ -130,7 +86,7 @@ public class ST10067556
         return find;
     }
 
-    public static String registerUser(boolean usernameInput,String userName, boolean passwordInput)
+    public static String registerUser(boolean usernameInput, boolean passwordInput)
     {
         int locator = 0;
         String sub1 = " ";
@@ -159,16 +115,21 @@ public class ST10067556
             message = "Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a number and a special character.";
         }
 
-//        if(passwordInput && usernameInput)
-//        {
-//            message = "Welcome " + sub1 + ", " + sub2 + " it is great to see you again";
-//        }
-//        else
-//        {
-//            message = "Username or password incorrect, please try again";
-//        }
-
         return message;
+    }
+
+    public static boolean loginUser(String userName, String password) throws IOException
+    {
+        FileWriter details = new FileWriter("LoginStorage.txt");
+        details.write(userName + "\n" + password);
+        details.close();
+
+        Scanner reader = new Scanner("filename.txt");
+        String user = reader.nextLine();
+        String pass = reader.nextLine();
+
+        return user == userName && pass == password;
+
     }
 
     public static String returnLoginStatus(boolean userNameB,String userName, boolean password)
@@ -186,7 +147,7 @@ public class ST10067556
                 locator++;
             }
             sub1 = userName.substring(0,locator);
-            sub2 = userName.substring(locator + 1,userName.length());
+            sub2 = userName.substring(locator + 1);
 
             message = "Welcome " + sub1 + ", " + sub2 + "it is great to see you again";
         }
@@ -201,23 +162,15 @@ public class ST10067556
 
     public static void main(String[] args)
     {
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Enter Username");
-//        String userNameInput = sc.next();
-//        System.out.println("Enter Password");
-//        String passwordInput = sc.next();
-//        System.out.println(registerUser(checkUserName(userNameInput), userNameInput, checkPasswordComplexity(passwordInput)));
-
         String passwordInput = "";
         String userNameInput = JOptionPane.showInputDialog("Enter Username");
 
         passwordInput = JOptionPane.showInputDialog("Enter Password");
 
-        JOptionPane.showMessageDialog(null, registerUser(checkUserName(userNameInput), userNameInput, checkPasswordComplexity(passwordInput)));
+        JOptionPane.showMessageDialog(null, registerUser(checkUserName(userNameInput), checkPasswordComplexity(passwordInput)));
 
         JOptionPane.showMessageDialog(null, returnLoginStatus(checkUserName(userNameInput),userNameInput, checkPasswordComplexity(passwordInput)));
-
-
+        //The questions and programming memo are the most ambiguous and limiting things I have ever seen btw :)
 
 
     }
