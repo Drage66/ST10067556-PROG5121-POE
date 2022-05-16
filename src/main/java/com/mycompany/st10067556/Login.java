b5390ca44dfa71486c5f112f473e82195a8364e8
Drage66 <nhlanhlazuma123@gmail.com>
2022-05-16 20:43:11 +0200

    Yes

---
+++ src/main/java/com/mycompany/st10067556/Login.java
@@ -0,0 +1,181 @@
+package com.mycompany.st10067556;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Login
+{
+    public static boolean checkUserName(String userName)
+    {
+        int locator = 0;
+        boolean find = false;// value to be returned can either be true or false
+
+        while (find == false && userName.length() <= 5)
+        {
+            if (userName.charAt(locator) == '_')// if statement that checks at increment char locations until it finds underscore
+            {
+                find = true;
+
+            }
+            else // if it doesnt find a undersocre at pos 0 then lookk for ir and pos 1, then pos 2
+            {
+                locator++;
+            }
+            if (locator == userName.length())
+            {
+                break;
+            }
+        }
+
+        return find;
+
+    }
+
+    public static boolean checkPasswordComplexity(String password)
+    {
+        int locator = 0;
+
+
+        boolean caps = false;
+        boolean digits = false;
+        boolean special = false;
+        boolean find = false; // value to be returned can either be true or false
+
+
+        while (find == false && password.length() >= 8)
+        {
+
+            if (locator == password.length())
+            {
+                break;
+            }
+
+            int charValue = password.charAt(locator);
+
+            if (charValue >= 65 && charValue <= 90)// looks for Caps and returns true if found
+            {
+                caps = true;
+            }
+
+
+            if (charValue >= 48 && charValue <= 57)
+            {
+                digits = true;
+            }
+
+
+            if (password.charAt(locator) == '@' || password.charAt(locator) == '$' || password.charAt(locator) == '~' || password.charAt(locator) == ':' || password.charAt(locator) == '#' || password.charAt(locator) == '!')
+            {
+                special = true;
+
+            }
+
+
+            if (special == true && digits == true && caps == true)
+            {
+                find = true;
+            }
+
+
+            locator++;
+        }
+        return find;
+    }
+
+    public static String registerUser(boolean usernameInput, boolean passwordInput)
+    {
+        int locator = 0;
+        String sub1 = " ";
+        String sub2 = " ";
+
+        String message = " ";
+        if(usernameInput)
+        {
+            message = "Username successfully captured";
+
+
+        }
+        else
+        {
+            message = "Username is not correctly formatted, please ensure that your username contains an underscore and is no more than 5 characters in length.";
+        }
+
+
+        if(passwordInput)
+        {
+            message = "Password successfully captured";
+
+        }
+        else
+        {
+            message = "Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a number and a special character.";
+        }
+
+
+
+        return message;
+    }
+
+    public static boolean loginUser(String userName, String password)
+    {
+        boolean un = false;
+        boolean pw = false;
+        boolean tog = false;
+
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\gamer\\Documents\\NetBeansProjects\\ST10067556\\src\\main\\java\\com\\mycompany\\st10067556\\LoginStorage.txt"));
+            if (br.readLine().equals(userName))
+            {
+                un = true;
+            }
+
+            if (br.readLine().equals(password))
+            {
+                pw = true;
+            }
+            br.close();
+        }
+        catch (Exception ignored)
+        {
+
+        }
+
+        if (un && pw)
+        {
+            tog = true;
+        }
+        return tog;
+
+
+
+    }
+
+    public static String returnLoginStatus(boolean verify,String userName)
+    {
+        int locator = 0;
+        String sub1 = " ";
+        String sub2 = " ";
+
+        String message = " ";
+        if(verify)
+        {
+
+            while(userName.charAt(locator) != '_')
+            {
+                locator++;
+            }
+            sub1 = userName.substring(0,locator);
+            sub2 = userName.substring(locator + 1);
+
+            message = "Welcome " + sub1 + ", " + sub2 + "it is great to see you again";
+        }
+        else
+        {
+            message = "Username or password incorrect, please try again";
+        }
+
+
+        return message;
+    }
+}
--- src/main/java/com/mycompany/st10067556/ST10067556.java
+++ src/main/java/com/mycompany/st10067556/ST10067556.java
@@ -3,176 +3,88 @@
 package com.mycompany.st10067556;
 
 import javax.swing.*;
+import javax.xml.validation.SchemaFactoryConfigurationError;
 import java.io.*;
+import java.util.Locale;
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.regex.*;
 
 
+
 public class ST10067556
 {
-    public static boolean checkUserName(String userName)
-    {
-        int locator = 0;
-        boolean find = false;// value to be returned can either be true or false
-
-        while (find == false && userName.length() <= 5)
-        {
-            if (userName.charAt(locator) == '_')// if statement that checks at increment char locations until it finds underscore
-            {
-                find = true;
-            }
-            else // if it doesnt find a undersocre at pos 0 then lookk for ir and pos 1, then pos 2
-            {
-                locator++;
-            }
-            if (locator == userName.length())
-            {
-                break;
-            }
-        }
-
-        return find;
-
-    }
-
-    public static boolean checkPasswordComplexity(String password)
-    {
-        int locator = 0;
-
-
-        boolean caps = false;
-        boolean digits = false;
-        boolean special = false;
-        boolean find = false; // value to be returned can either be true or false
-
-
-        while (find == false && password.length() >= 8)
-        {
-
-            if (locator == password.length())
-            {
-                break;
-            }
-
-            int charValue = password.charAt(locator);
-
-            if (charValue >= 65 && charValue <= 90)// looks for Caps and returns true if found
-            {
-                caps = true;
-            }
-
-
-            if (charValue >= 48 && charValue <= 57)
-            {
-                digits = true;
-            }
-
-
-            if (password.charAt(locator) == '@' || password.charAt(locator) == '$' || password.charAt(locator) == '~' || password.charAt(locator) == ':' || password.charAt(locator) == '#' || password.charAt(locator) == '!')
-            {
-                special = true;
-
-            }
-
-
-            if (special == true && digits == true && caps == true)
-            {
-                find = true;
-            }
-
-
-            locator++;
-        }
-        return find;
-    }
-
-    public static String registerUser(boolean usernameInput, boolean passwordInput)
-    {
-        int locator = 0;
-        String sub1 = " ";
-        String sub2 = " ";
-
-        String message = " ";
-        if(usernameInput)
-        {
-            message = "Username successfully captured";
-
-
-        }
-        else
-        {
-            message = "Username is not correctly formatted, please ensure that your username contains an underscore and is no more than 5 characters in length.";
-        }
-
-
-        if(passwordInput)
-        {
-            message = "Password successfully captured";
-
-        }
-        else
-        {
-            message = "Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a number and a special character.";
-        }
-
-        return message;
-    }
-
-    public static boolean loginUser(String userName, String password) throws IOException
-    {
-        FileWriter details = new FileWriter("LoginStorage.txt");
-        details.write(userName + "\n" + password);
-        details.close();
-
-        Scanner reader = new Scanner("filename.txt");
-        String user = reader.nextLine();
-        String pass = reader.nextLine();
-
-        return user == userName && pass == password;
-
-    }
-
-    public static String returnLoginStatus(boolean userNameB,String userName, boolean password)
-    {
-        int locator = 0;
-        String sub1 = " ";
-        String sub2 = " ";
-
-        String message = " ";
-        if(userNameB && password)
-        {
-
-            while(userName.charAt(locator) != '_')
-            {
-                locator++;
-            }
-            sub1 = userName.substring(0,locator);
-            sub2 = userName.substring(locator + 1);
-
-            message = "Welcome " + sub1 + ", " + sub2 + "it is great to see you again";
-        }
-        else
-        {
-            message = "Username or password incorrect, please try again";
-        }
-
-
-        return message;
-    }
+    public static int totalDuration;
+    public static int duration;
 
     public static void main(String[] args)
     {
+        //String variable declarations
         String passwordInput = "";
         String userNameInput = JOptionPane.showInputDialog("Enter Username");
 
         passwordInput = JOptionPane.showInputDialog("Enter Password");
 
-        JOptionPane.showMessageDialog(null, registerUser(checkUserName(userNameInput), checkPasswordComplexity(passwordInput)));
+        JOptionPane.showMessageDialog(null, Login.registerUser(Login.checkUserName(userNameInput),Login.checkPasswordComplexity(passwordInput)));
+        JOptionPane.showMessageDialog(null, Login.returnLoginStatus(Login.loginUser(userNameInput, passwordInput),userNameInput));
 
-        JOptionPane.showMessageDialog(null, returnLoginStatus(checkUserName(userNameInput),userNameInput, checkPasswordComplexity(passwordInput)));
-        //The questions and programming memo are the most ambiguous and limiting things I have ever seen btw :)
+        if(Login.loginUser(userNameInput, passwordInput))
+        {
+            String option = " ";
+            while (!option.equals("3"))
+            {
+                option = JOptionPane.showInputDialog("Welcome to EasyKanban" +
+                        "\nOption1) Add tasks" +
+                        "\nOption2) Show Report" +
+                        "\nOption3) Quit");
+
+                if (option.equals("1"))
+                {
+                    totalDuration = 0;
 
 
+                    String numberOfTask = JOptionPane.showInputDialog("How many tasks do you wish to enter?");
+                    int InumberOfTask = Integer.parseInt(numberOfTask);
+
+                    for (int i = 0; i < InumberOfTask; i++)
+                    {
+                        String taskName = JOptionPane.showInputDialog("Task Name");
+                        String taskNumber = Integer.toString(i+1);
+                        JOptionPane.showMessageDialog(null, taskNumber);
+                        String taskDesc = JOptionPane.showInputDialog("Task Description");
+                        if(Task.checkTaskDescription(taskDesc))
+                        {
+                            while(Task.checkTaskDescription(taskDesc))
+                            {
+                                taskDesc = JOptionPane.showInputDialog(null, "Task description may not be longer than 50 characters","", JOptionPane.ERROR_MESSAGE);
+                            }
+                        }
+                        String devDetail = JOptionPane.showInputDialog("Developer Details");
+                        String taskDuration = JOptionPane.showInputDialog("Task Duration");
+                        duration = Integer.parseInt(taskDuration);
+                        String taskID = Task.createTaskID(taskName,taskNumber,devDetail);
+
+                        String taskStatus = JOptionPane.showInputDialog("Option1) To Do" +
+                                "\nOption2) Done" +
+                                "\nOption3) Doing");
+
+
+                        JOptionPane.showMessageDialog(null, Task.printTaskDetails(taskStatus,devDetail,taskNumber,taskName,taskDesc,taskID,duration));
+                        Task.returnTotalHours();
+
+
+                    }
+                    JOptionPane.showMessageDialog(null, "The Total amount of hours spent on task is " + Task.returnTotalHours() + " hours!");
+
+                }
+
+                if (option.equals("2"))
+                {
+                    JOptionPane.showMessageDialog(null, "Coming Soon :)");
+                }
+            }
+
+        }
     }
 
 }
